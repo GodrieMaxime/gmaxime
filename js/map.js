caa660bf0a8e4177688bb8677ce7338398145ff2
Unknown <godrie.maxime@gmail.com>
2019-02-25 18:49:51 +1100

    change tile layer

--- js/map.js
+++ js/map.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
    // Initialize the map and assign it to a variable for later use
-var map = L.map('map', {
-    // Set latitude and longitude of the map center (required)
-    center: [-33.854816, 151.216454],
-    // Set the initial zoom level, values 0-18, where 0 is most zoomed-out (required)
-    zoom: 5
-});
+    var map = L.map('map', {
+        // Set latitude and longitude of the map center (required)
+        center: [-33.854816, 151.216454],
+        // Set the initial zoom level, values 0-18, where 0 is most zoomed-out (required)
+        zoom: 5
+    });
 
-// adding marker
-var marker = L.marker([-33.854816, 151.216454]).addTo(map);
+    // adding marker
+    var marker = L.marker([-33.854816, 151.216454]).addTo(map);
 
-// Create a Tile Layer and add it to the map
-var tiles = new L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png').addTo(map);
+    // Create a Tile Layer and add it to the map
+    var tiles = new L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(map);
 });
